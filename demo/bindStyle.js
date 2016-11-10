01eba33d663e0747eb4cfdca335353dd58361466
wendyLian <wendy_5102@163.com>
2016-11-10 09:06:04 +0800

    bindStyle

--- demo/bindStyle.js
+++ demo/bindStyle.js
@@ -3,7 +3,19 @@ var bindStyle = new Vue({
 	el: '#bindStyle',
 	data: function () {
 		return {
-			isActive: true
+			isActive: true,
+			classObject: {
+				isActive: true,
+				error: null
+			}
+		}
+	},
+	computed: {
+		classObject: function(){
+			return {
+				active: this.isActive && !this.error,
+				'text-danger': this.error && this.error.type === 'fatal'
+			}
 		}
 	}
 })
